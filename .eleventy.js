461208f851d1ce5daba40ff8810a903d71050ecf
countff <countff@n-t.io>
2023-02-06 10:32:22 +0300

    uploads directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
 
   // Folders to copy to output folder
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("uploads");
   return {
     pathPrefix: "doc"/*,
     dir: {
