34252e9efca42b9c9cc523f4924cbc1a01826783
countff <countff@n-t.io>
2023-05-03 16:15:16 +0300

    minor file fix

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,17 @@ module.exports = function (eleventyConfig) {
   // Folders to copy to output folder
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("uploads");
-/*  return {
-    pathPrefix: "doc"/*,
-    dir: {
-      input: "src",
-      output: "www",
-    }*
-  };*/
+  return {
+    pathPrefix: "doc"
+  };
+
+
+//  return {
+//    pathPrefix: "doc"/*,
+//    dir: {
+//      input: "src",
+//      output: "www",
+//    }*/
+  };
 
 };
